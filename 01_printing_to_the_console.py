a4e962a9fc694289d19dadf341fe1ff1005de03c
Vanda Huszak <vanda.soltis@gmail.com>
2023-07-09 22:32:26 +0200

    Printing, Input functions and string manipulation

---
+++ 01_printing_to_the_console.py
@@ -0,0 +1,17 @@
+print("Hello world!")
+
+print('---------------------------')
+
+print("print(\"Hello world!\")")
+
+print('---------------------------')
+
+print("First line\nSecond line, separated by line break")
+
+print('---------------------------')
+
+print("\ttab to the right")
+
+print('---------------------------')
+
+print(233)
---
+++ 03_input_function.py
@@ -0,0 +1,4 @@
+print("Hello " + input("What is your name?")) # Comment: Code always executes from the in to the out
+
+name = input("What is your name?")
+print("Hello " + name)
