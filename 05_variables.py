6c75b15312468d58eda90ee8448513e29bc511ac
Vanda Huszak <vanda.soltis@gmail.com>
2023-07-10 17:29:30 +0200

    len function and variables

---
+++ 05_variables.py
@@ -0,0 +1,12 @@
+name = input("What is your name?")
+print(name)
+
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+name = input("What is your name?")
+lenght = len(name)
+print(lenght)
