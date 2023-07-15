0ba4df90f51cc620a765514ec7333f46a5aeaa34
Vanda Huszak <vanda.soltis@gmail.com>
2023-07-15 18:26:39 +0200

    Typo fix

--- 01_working_with_variables_in_python_to_manage_data/07_band_name_generator.py
+++ 01_working_with_variables_in_python_to_manage_data/07_band_name_generator.py
@@ -5,7 +5,7 @@ city = input("Where did you grow up?\n") # Option 1 for new line
 print("What is your pet name?") # Option 2 for new line
 pet_name = input()
 
-brand_name = city + " " + pet_name # One line should represent one step, so this is better than: print("Your band name is:\n" + city + " " + pet_name)
+band_name = city + " " + pet_name # One line should represent one step, so this is better than: print("Your band name is:\n" + city + " " + pet_name)
 
 print("Your band name is:")
-print(brand_name)
+print(band_name)
