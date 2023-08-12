3324447157634b17353b9057c731d7efd749966c
Vanda Huszak <vanda.soltis@gmail.com>
2023-08-12 21:30:21 +0200

    For loops and the range excercises

---
+++ 05_loops/06_for_loops_and_the_range_function.py
@@ -0,0 +1,17 @@
+#for loop with range
+#printing 1 to 9 
+for number in range(1, 10):
+    print(number)
+
+#printing 1 to 10
+for number in range(1, 11):
+    print(number)
+
+#step number
+for number in range(1, 11, 3):
+    print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
